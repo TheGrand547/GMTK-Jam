5f07e1c3798c08fc3a24646f3a6e80ff596e367d
samf547 <sdfisher79@gmail.com>
2021-06-11 16:35:36 -0400

    Color

--- main.lua
+++ main.lua
@@ -1,19 +1,25 @@
 -- Called once
 function love.load()
-  x = 10
-  y = 10
+  blocks = {}
+  x = 300
+  y = 300
   t = 0
+  deltat = 0
+  love.window.requestAttention()
+  love.graphics.setBackgroundColor(1, 1, 0)
 end
 
 -- Called continuously
-function love.update()
-  x = x + 5 * math.cos(t)
-  y = y + 5 * math.sin(t)
-  t = t + .5
+function love.update(dt)
+  x = x + 100 * math.cos(t) * dt
+  y = y + 100 * math.sin(t) * dt 
+  t = t + 10 * math.pi / 360
+  deltat = dt
 end
 
 -- Called continuously
 function love.draw()
-    love.graphics.print("Hel3lo World!", 100, 100)
+    love.graphics.setColor(1, 0, 0)
     love.graphics.rectangle("fill", x, y, 40, 50)
+    love.graphics.print(1 / deltat, 100, 100)
 end
